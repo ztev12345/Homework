a451e1286d760ff599fab0a5795e3a157450d0df
Andy Xiao <zxc5900056@gmail.com>
2023-06-12 15:44:10 +0800

    加入專案檔案。

--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Alarm());
+            Application.Run(new Main());
         }
     }
 }
